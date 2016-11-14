a9f63ce2a471cc83e9f94a84122e726c2b3e15f4
Zhisong Zhang <stonechueng@hotmail.com>
2016-11-14 14:26:49 +1100

    display list of customers with the movie added customer class added viewModel folder and class

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,28 +15,28 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Die Hard!" };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                    new Customer { Name="Customer 1" },
+                    new Customer { Name="Customer 2" },
+                    new Customer { Name="Customer 3" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
-
-        //public ActionResult Edit(int id)
-        //{
-        //    return Content("id= " + id);
-        //}
-
-        //// Movie
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (string.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-        //    return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        //}
-
+        
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
         }
+
+
     }
 }
