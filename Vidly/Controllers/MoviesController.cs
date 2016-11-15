b07634b73b1de0b35f6fca0d4dd160c6cb600d6c
Zhisong Zhang <stonechueng@hotmail.com>
2016-11-15 11:37:04 +1100

    Modification of Exec 3 added detail page in movie added actionLink to movie index added linq function to movie controller modified NavBar

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,25 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Linq;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Die Hard!" };
-
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                    new Customer { Name="Customer 1" },
-                    new Customer { Name="Customer 2" },
-                    new Customer { Name="Customer 3" }
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -30,13 +42,15 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
-        
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+
+        public ActionResult Details(int id)
         {
-            return Content(year + "/" + month);
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
-
-
     }
 }
