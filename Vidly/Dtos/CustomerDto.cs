f5e8bbd23cde6a6724ce7cea87bbb127b70454b3
Zhisong Zhang <stonechueng@hotmail.com>
2016-11-18 15:01:32 +1100

    Added automapper 4.1 Added customerDto upated customer webapi using Dto and automapper formation json response to camel case

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(225)]
+        public string Name { get; set; }
+
+        public bool IsSubcribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Dob { get; set; }
+    }
+}
